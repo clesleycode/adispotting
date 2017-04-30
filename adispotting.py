f168791562799b46a37252aa5b672662bc2077b8
Lesley Cordero <lc2958@columbia.edu>
2017-04-30 15:12:41 -0400

    handles images now

--- adispotting.py
+++ adispotting.py
@@ -4,7 +4,6 @@ from slackclient import SlackClient
 
 BOT_ID = os.environ.get("BOT_ID")
 AT_BOT = "<@" + BOT_ID + ">"
-EXAMPLE_COMMAND = "do"
 
 slack_client = SlackClient(os.environ.get("SLACK_BOT_TOKEN"))
 mems = slack_client.api_call(
@@ -25,34 +24,33 @@ class ADISpotting:
         self.users[user] = self.users[user] + points
         if self.users[self.winner] < self.users[user]:
             self.winner = user
+    
+    def get_points(self, user):
+        return(self.users[user])
+
 
 adispot = ADISpotting()
 for i in mems:
     adispot.add_user(i)
 
-def handle_command(command, channel):
-    # receives commands at bot and determines validity
-    response = "Not sure what you mean."
-    if command.startswith(EXAMPLE_COMMAND): 
-        response = "Cool"
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
-                return(output['user'],output['text'].split(AT_BOT)[1].strip().lower(), output['channel'])
-    return(None,None,None)
+                slack_client.api_call("chat.postMessage", text="What do you want?", channel= output['channel'], as_user=True)
+            elif output and 'user_id' in output and output['type'] == 'file_shared':
+                adispot.add_points(output['user_id'], 5)
+                slack_client.api_call("chat.postMessage", channel="C57D2GXM4", text=output['user_id'] + " now has " + str(adispot.get_points(output['user_id'])) + " points!", as_user=True)
+
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1
     if slack_client.rtm_connect():
         print("ADISpotting connected and running!")
         while True:
-            user, command, channel = parse_slack_output(slack_client.rtm_read())
-            if user and command and channel:
-                handle_command(command,channel)
+            parse_slack_output(slack_client.rtm_read())
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid slack token or bot ID")
